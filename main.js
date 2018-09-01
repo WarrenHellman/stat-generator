51c02b39bc5d7e1feaaceb5049a096a1280b44f0
Warren <w.e.hellman@gmail.com>
2018-08-31 19:59:11 -0700

    Populating output form

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Bugs- Delete any <li> dynamic creator if number is changed
+
 const attributeNum = document.getElementById("attributeNum");
 const aboutNum = document.getElementById("aboutNum");
 
@@ -8,7 +10,6 @@ attributeNum.addEventListener('keyup', function(e){
   }
   else {
     let fields = attributeNum.value;
-    console.log(fields)
     for (let i=0; i<fields; i++){
       // Creates label input field nodes for name
       const attributeLabel = document.createElement("label")
@@ -30,30 +31,59 @@ attributeNum.addEventListener('keyup', function(e){
       valueName.name = "Value "+(i+1);
 
       const attributeField = document.querySelector("#attributeField")
+
+      const breaks = document.createElement("br");
       attributeField.appendChild(attributeLabel);
       attributeField.appendChild(attributeName);
       attributeField.appendChild(valueLabel);
       attributeField.appendChild(valueName);
+      attributeField.appendChild(breaks);
+      attributeField.appendChild(breaks);
     }
   }
 })
 
+// Creates Dynamic field for about values
 aboutNum.addEventListener('keyup', function(e){
   if (aboutNum.value < 0 || aboutNum.value > 7){
     alert("Please select a value between 0-7");
   }
   else {
     let fields = aboutNum.value;
-    console.log(fields)
     for (let i=0; i<fields; i++){
       const aboutLabel = document.createElement("label")
       aboutLabel.setAttribute("for", "About "+(i+1));
       aboutLabel.innerHTML = "About "+(i+1)+":";
+
       const aboutName = document.createElement("input")
       aboutName.type = "text";
       aboutName.name = "About "+(i+1);
+      aboutName.className = "wideInput";
+
+      const breaks = document.createElement("br");
+      
       document.querySelector("#aboutField").appendChild(aboutLabel);
       document.querySelector("#aboutField").appendChild(aboutName);
+      document.querySelector("#aboutField").appendChild(breaks);
+      document.querySelector("#aboutField").appendChild(breaks);
     }
   }
 })
+
+let create = document.getElementById("create");
+
+create.addEventListener('click', generateForm);
+
+function generateForm(){
+  let titleValue = document.getElementById("titleName").value;
+  let title = document.getElementById("title");
+  title.innerHTML = titleValue;
+
+  let subtitleValue = document.getElementById("subtitleName").value;
+  let subtitle = document.getElementById("subtitle");
+  subtitle.innerHTML = subtitleValue;
+
+  let aboutField = document.getElementById('aboutField');
+
+  console.log(aboutField.children.length);
+}
