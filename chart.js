4cc2bf3bd6ebd851e9c09bea8f998806487db00e
Warren <w.e.hellman@gmail.com>
2018-09-19 16:03:38 -0700

    Updated layout

--- chart.js
+++ chart.js
@@ -10,7 +10,7 @@ function buildChart(object){
   var ctx = document.getElementById('myChart').getContext('2d');
   var chart = new Chart(ctx, {
     // The type of chart we want to create
-    "type":"radar","data":{"labels":object.labels,"datasets":[{"data":object.values,"fill":true,"backgroundColor":"rgba(255, 99, 132, 0.2)","borderColor":"rgb(255, 99, 132)","pointBackgroundColor":"rgb(255, 99, 132)","pointBorderColor":"#fff","pointHoverBackgroundColor":"#fff","pointHoverBorderColor":"rgb(255, 99, 132)"}]},"options":{"scale": {"ticks": {"suggestedMin":0, "fontColor": "orange"},"angleLines":{"color":"black"}, "pointLabels":{"fontColor":"black", "fontFamily":"Cinzel", "fontSize":20}},"legend":{"display":false},"elements":{"line":{"tension":0,"borderWidth":3}}}
+    "type":"radar","data":{"labels":object.labels,"datasets":[{"data":object.values,"fill":true,"backgroundColor":"rgba(255, 99, 132, 0.2)","borderColor":"rgb(255, 99, 132)","pointBackgroundColor":"rgb(255, 99, 132)","pointBorderColor":"#fff","pointHoverBackgroundColor":"#fff","pointHoverBorderColor":"rgb(255, 99, 132)"}]},"options":{"scale": {"ticks": {"suggestedMin":0, "fontColor": "orange"},"angleLines":{"color":"black"}, "pointLabels":{"fontColor":"black", "fontFamily":"Cinzel", "fontSize":13}},"legend":{"display":false},"elements":{"line":{"tension":0,"borderWidth":3}}}
   });
 }
 
